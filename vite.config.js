f64b6ae0de89df09b4c6d99029761596e51c6f46
thorgal08 <tomaspax@gmail.com>
2025-07-07 11:24:19 -0400

    pwa implementado

--- vite.config.js
+++ vite.config.js
@@ -1,7 +1,33 @@
-import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react'
+import { defineConfig } from 'vite';
+import react from '@vitejs/plugin-react';
+import { VitePWA } from 'vite-plugin-pwa';
 
 // https://vite.dev/config/
 export default defineConfig({
-  plugins: [react()],
-})
+  plugins: [
+    react(),
+    VitePWA({
+      registerType: 'autoUpdate',
+      manifest: {
+        short_name: 'Vales',
+        name: 'Gestión de Vales',
+        icons: [
+          {
+            src: 'icon-192.png',
+            sizes: '192x192',
+            type: 'image/png'
+          },
+          {
+            src: 'icon-512.png',
+            sizes: '512x512',
+            type: 'image/png'
+          }
+        ],
+        start_url: '.',
+        display: 'standalone',
+        theme_color: '#2563eb',
+        background_color: '#ffffff'
+      }
+    })
+  ]
+});
