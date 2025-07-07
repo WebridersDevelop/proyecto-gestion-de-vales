188873ac2c93e55698eda258199b35bee5296ddb
thorgal08 <tomaspax@gmail.com>
2025-07-06 22:04:05 -0400

    correciones

--- peluqueria-app/src/pages/AdminOnly.jsx
+++ peluqueria-app/src/pages/AdminOnly.jsx
@@ -3,7 +3,7 @@ import { useAuth } from '../context/AuthContext';
 
 function AdminOnly({ children }) {
   const { rol, loading } = useAuth();
-  if (loading) return <div>Cargando...</div>;
+  if (loading) return <div>Cargando....</div>;
   if (rol !== "admin") return <div>No autorizado</div>;
   return children;
 }
