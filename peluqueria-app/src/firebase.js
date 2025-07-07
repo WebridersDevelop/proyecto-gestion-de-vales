8f179a3578bafd22911326a026f09f8f568b8479
thorgal08 <tomaspax@gmail.com>
2025-07-06 21:26:20 -0400

    seguridad implementada

--- peluqueria-app/src/firebase.js
+++ peluqueria-app/src/firebase.js
@@ -2,15 +2,15 @@ import { initializeApp } from "firebase/app";
 import { getAuth } from "firebase/auth";
 import { getFirestore } from "firebase/firestore";
 
-// Tu configuración de Firebase
+// Configuración de Firebase usando variables de entorno
 const firebaseConfig = {
-  apiKey: "AIzaSyCiQVRoRF5tKIVtC3vzPPUVO2ALyecytE8",
-  authDomain: "proyecto-vales.firebaseapp.com",
-  projectId: "proyecto-vales",
-  storageBucket: "proyecto-vales.appspot.com",
-  messagingSenderId: "1016151478605",
-  appId: "1:1016151478605:web:9c90aea85c314ca3fd6704",
-  measurementId: "G-HQCEB2HY41"
+  apiKey: import.meta.env.VITE_FIREBASE_API_KEY,
+  authDomain: import.meta.env.VITE_FIREBASE_AUTH_DOMAIN,
+  projectId: import.meta.env.VITE_FIREBASE_PROJECT_ID,
+  storageBucket: import.meta.env.VITE_FIREBASE_STORAGE_BUCKET,
+  messagingSenderId: import.meta.env.VITE_FIREBASE_MESSAGING_SENDER_ID,
+  appId: import.meta.env.VITE_FIREBASE_APP_ID,
+  measurementId: import.meta.env.VITE_FIREBASE_MEASUREMENT_ID
 };
 
 // Inicializa Firebase
